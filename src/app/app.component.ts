692eb67a35228a0ae96c801f3536234b4ba1a15e
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-05-03 20:03:54 -0400

    new project

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'app';
+// }
+
+
+
+import { Component, OnInit } from '@angular/core';
+
+declare var myAct: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+
+  ngOnInit() {
+    myAct();
+  }
+}
+
