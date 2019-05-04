ccd54d530cce867143ac879562f339e460f1f0e8
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-05-04 08:35:22 -0400

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,66 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	title = "MATCHMAKER";
-	paragraph = "What careers match your interests?";
-	btn01 = "Accountant";
-	btn02 ="Creativity";
-	tooltip01 = "Like Math?";
-	tooltip02 = "You are very Creative"
+  title = "MATCHMAKER";
+  paragraph = "What careers match your interests?";
+  btn01 = "Accountant";
+  btn02 = "Creativity";
+  tooltip01 = "Like Math?";
+  tooltip02 = "You are very Creative"
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-	  const html = document.getElementsByTagName("HTML")[0];
-	  const classname = document.getElementsByClassName("button");
-	  const thisEl = Math.round(classname[0].getBoundingClientRect().top);
+    const html = document.getElementsByTagName("HTML")[0];
+    const classname = document.getElementsByClassName("button");
+    const thisEl = Math.round(classname[0].getBoundingClientRect().top);
 
-	  const hideTooltip = function(e) {
-	  	Array.from(classname).forEach(function(element){
-	      element.classList.remove('tooltop--open');
-	    });
-	    e.stopPropagation();
-	  };
+    const hideTooltip = function(e) {
+      Array.from(classname).forEach(function(element) {
+        element.classList.remove('tooltop--open');
+      });
+      e.stopPropagation();
+    };
 
-	  const showTooltip = function(e) {
-	    Array.from(classname).forEach(function(element){
-	      element.classList.remove('tooltop--open');
-	    });
-	    this.classList.add('tooltop--open');
-	    e.stopPropagation();
-	  }
+    const showTooltip = function(e) {
+      Array.from(classname).forEach(function(element) {
+        element.classList.remove('tooltop--open');
+      });
+      this.classList.add('tooltop--open');
+      e.stopPropagation();
+    }
 
-	  Array.from(classname).forEach(function(element){
-	    element.addEventListener("click", showTooltip);
-	  });
-	  html.addEventListener("click", hideTooltip);
+    Array.from(classname).forEach(function(element) {
+      element.addEventListener("click", showTooltip);
+    });
+    html.addEventListener("click", hideTooltip);
 
-	  window.onscroll = function() {  
-	    if( window.scrollY >= ( thisEl + 40 ) ){     
-	      Array.from(classname).forEach(function(element){
-	        element.classList.add('button__reach');
-	      });
-	    }else{
-	      Array.from(classname).forEach(function(element){
-	        element.classList.remove('button__reach');
-	      });
-	    }
-	  }
+    window.onscroll = function() {
+      if (window.scrollY >= (thisEl + 40)) {
+        Array.from(classname).forEach(function(element) {
+          element.classList.add('button__reach');
+        });
+      } else {
+        Array.from(classname).forEach(function(element) {
+          element.classList.remove('button__reach');
+        });
+      }
+    }
 
-	  document.addEventListener('keyup', function (event) {
-			if (event.defaultPrevented) {
-		    return;
-			}
+    document.addEventListener('keyup', function(event) {
+      if (event.defaultPrevented) {
+        return;
+      }
 
-			var key = event.key || event.keyCode;
+      var key = event.key || event.keyCode;
 
-			if (key === 'Escape' || key === 'Esc' || key === 27) {
-		    Array.from(classname).forEach(function(element){
-		  		element.classList.remove('tooltop--open');
-				});
-			}
-		});
-	  
+      if (key === 'Escape' || key === 'Esc' || key === 27) {
+        Array.from(classname).forEach(function(element) {
+          element.classList.remove('tooltop--open');
+        });
+      }
+    });
+
   }
 
 }
